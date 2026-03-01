93a71f87f261519ebb2a84ecc006725872890f3c
Prathamesh-Kadu <prathameshkadu09@gmail.com>
2026-03-01 18:28:32 +0530

    feat(auth): implement user registration & login with JWT, role hierarchy, validation

---
+++ Backend/TaskSphere/src/main/java/com/prathamesh/tasksphere/security/JwtService.java
@@ -0,0 +1,43 @@
+package com.prathamesh.tasksphere.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.prathamesh.tasksphere.model.User;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+	@Value("${jwt.secret-key}")
+    private String SECRET;
+    private final long EXPIRATION = 1000 * 60 * 60;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
